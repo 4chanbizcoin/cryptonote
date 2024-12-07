1a0882bcee54a5c62ec25a208b2cf7e59fd2070b
4chanbizcoin <greenpoisonjb@icloud.com>
2024-12-07 19:23:26 +1100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "172.17.0.2:4200",
-  "172.17.0.7:4201",
+  "192.168.1.122:4200",
+  "10.0.2.15:4201",
 };
 
 struct CheckpointData {
